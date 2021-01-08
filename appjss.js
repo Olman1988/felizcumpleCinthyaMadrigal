bdd17eab38533e0b5eb327ae3bf6e97c2715bf8d
Olman1988 <58829857+Olman1988@users.noreply.github.com>
2021-01-08 14:56:52 -0600

    Add files via upload

--- appjss.js
+++ appjss.js
@@ -2,7 +2,7 @@ function mostrar(){
    
     
     document.getElementById("princ").style.display="block";
-   document.getElementById("princ2").style.display="block";
+    document.getElementById("princ2").style.display="block";
     document.getElementById("inicio").style.background="linear-gradient(to right, rgba(242, 112, 156, 0.6), rgba(255, 148, 114, 0.6)), url('tenor.gif')"; 
     document.getElementById("corazon").style.display="none";
     document.getElementById('player').play();
